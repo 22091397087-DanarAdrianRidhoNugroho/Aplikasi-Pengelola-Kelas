d37acadc18c446b98f8d0a670a28c92c016cf218
22091397087-DanarAdrianRidhoNugroho <adrianridho.id@gmail.com>
2024-06-15 13:25:59 +0700

    frist commit

---
+++ koordinator/footer.php
@@ -0,0 +1,7 @@
+</div>
+</div>
+<div class="column">
+    <h2><span>CopyRight &copy; 2024 - Sistem Informasi - Kelompok 3</span></h2>
+</div>
+</body>
+</html>
---
+++ koordinator/index.php
@@ -0,0 +1,52 @@
+<?php
+session_start(); // Memulai sesi
+include('../include/koneksi.php');
+
+if (!isset($_SESSION['email'])) {
+    // Jika sesi tidak ada, arahkan ke halaman login
+    header("Location: ../login-koordinator.php");
+    exit();
+}
+
+$email = $_SESSION['email']; // mengambil email dari session yang login
+
+// Query untuk mengecek email
+$cek = $koneksi->query("SELECT * FROM koordinator WHERE email='$email'");
+if ($cek->num_rows == 0) {
+    header("Location: ../login-koordinator.php");
+    exit();
+} else {
+    $row = $cek->fetch_assoc();
+    $nama = $row['nama'];
+}
+?>
+
+<?php include('header.php'); // Menyertakan header ?>
+
+<style>
+.halaman1 {
+padding: 20px;
+}
+.isi-halaman {
+    background-color: #EEEEEE;
+    padding: 20px;
+    border-radius: 10px;
+}
+.artikel {
+    margin-bottom: 20px;
+}
+.artikel h3 {
+    margin: 0;
+    font-size: 30px;
+}
+</style>
+<div class="halaman1">
+    <div class="isi-halaman">
+        <div class="isi">
+            <div class="artikel">
+                <h3>Selamat Datang <?php echo $nama; ?>.....</h3>
+            </div>
+        </div>
+        <div class="hapus"></div>
+    </div>
+<?php include('footer.php'); // Menyertakan footer ?>
